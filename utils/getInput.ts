6e372a45a3c4316f68bca35c463e548dc185bbe8
Markus Unger <schaulustig23@gmail.com>
2023-12-02 13:05:17 +0100

    fix: input file name

--- utils/getInput.ts
+++ utils/getInput.ts
@@ -1,6 +1,6 @@
 import fs from 'fs/promises';
 
 export const getInput = async (options: { test: boolean } = { test: false }): Promise<string[]> => {
-    const input = await fs.readFile('input' + options.test ? '_test' : '', 'utf-8');
+    const input = await fs.readFile('input' + (options.test ? '_test' : ''), 'utf-8');
     return input.split('\n');
 };
