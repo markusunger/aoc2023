af85a86a4d5c2e7bfc4ef0b16c58767c04d4d513
Markus Unger <schaulustig23@gmail.com>
2023-12-21 17:41:12 +0100

    feat: add split option to getInput

--- utils/getInput.ts
+++ utils/getInput.ts
@@ -1,6 +1,17 @@
 import fs from 'fs/promises';
 
-export const getInput = async (options: { test: boolean } = { test: false }): Promise<string[]> => {
-    const input = await fs.readFile('input' + (options.test ? '_test' : ''), 'utf-8');
-    return input.split('\n');
+interface GetInputOptions {
+    test?: boolean;
+    splitOn?: string;
+}
+
+const GetInputOptionsDefault = {
+    test: false,
+    splitOn: '\n',
+    transform: (input: string) => input,
+};
+
+export const getInput = async ({ test, splitOn }: GetInputOptions = {}): Promise<string[]> => {
+    const input = await fs.readFile('input' + (test ? '_test' : ''), 'utf-8');
+    return input.split(splitOn || GetInputOptionsDefault.splitOn);
 };
